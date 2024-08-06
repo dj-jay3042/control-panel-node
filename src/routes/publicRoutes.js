49e6fe52fc344dc01153bdab575b787c979ae97d
Jay Chauhan <142774567+SF-dev1@users.noreply.github.com>
2024-08-06 12:08:06 +0530

    update

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const LoginController = require('../controllers/LoginController');
 const RoutesController = require('../controllers/RoutesController');
+const SmsController = require('../controllers/SmsController');
 const router = express.Router();
 
 /*********************************************************************************************
